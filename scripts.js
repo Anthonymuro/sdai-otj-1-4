e5a9922f476ee34876d4b86a28fc9b11d3379777
Anthony muro <117108039+Anthonymuro@users.noreply.github.com>
2024-08-27 22:51:38 +0000

    cleaned up hmtl and css

--- scripts.js
+++ scripts.js
@@ -5,3 +5,25 @@ function saletotal (item1, item2, item3) {
 }
 //call of the function 
 console.log(saletotal (2.49, 5.25, 4.99)) //13.23
+
+
+//funtion to calculate the area of a rectangle
+//formula = l * w
+//definition of the funtion
+
+function arearectangle (length, width) {
+    return length * width
+}
+
+// call funtion  to use
+console.log(arearectangle(10, 20))
+
+//function to divide two numbers
+function divide (num1, num2) {
+    return num1/num2
+}
+
+//call function to use
+console.log(divide(12, 2)) //divide facts
+console.log(divide(12, 0)) //???
+console.log(divide(13, 2)) //float / decimal
