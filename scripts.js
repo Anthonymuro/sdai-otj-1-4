03b14a73f952ae40855df2921fe84e09949ac875
Anthony muro <117108039+Anthonymuro@users.noreply.github.com>
2024-08-27 03:26:18 +0000

    did some js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("my javascripts works!")
+//calculate and return the total for a sale
+
+function saletotal (item1, item2, item3) {
+    return item1 + item2 + item3
+}
+//call of the function 
+console.log(saletotal (2.49, 5.25, 4.99)) //13.23
